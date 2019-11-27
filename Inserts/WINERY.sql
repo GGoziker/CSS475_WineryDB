894b20995d5ca506a257fb61094aa6ab9fb87eec
avmoroz <andrei.nexus@gmail.com>
2019-11-27 01:16:08 -0800

    Created inserts, modified some of the database setup to account for nulls and %s

---
+++ Inserts/WINERY.sql
@@ -0,0 +1,6 @@
+INSERT INTO WINERY VALUES(1,'Columbia Winery','Ursula','Lagadu',695051156);
+INSERT INTO WINERY VALUES(2,'Long Shadows','Elsa','Golitzin',512628281);
+INSERT INTO WINERY VALUES(3,'Chateau Ste. Michelle','Peter','Beswell',984680868);
+INSERT INTO WINERY VALUES(4,'Avennia','Alexander ','Vinall',272414269);
+INSERT INTO WINERY VALUES(5,'Abeja','Thomas','Blonfield',536364213);
+INSERT INTO WINERY VALUES(6,'Delille Cellars','Christopher','McKevany',437839222);
