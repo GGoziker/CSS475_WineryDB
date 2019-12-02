a51162becdb7eacccd8b23d3b97fd375bf7493b7
Camila Valdebenito <cfvb22@uw.edu>
2019-12-02 15:14:26 -0800

    Update Sqlite3_updated_database_setup.sql

--- Database_setup/Sqlite3_updated_database_setup.sql
+++ Database_setup/Sqlite3_updated_database_setup.sql
@@ -134,12 +134,12 @@ Primary key(Cellar_ID),
 Foreign key(Winery_ID) references WINERY(Winery_ID)
 );
 
-INSERT INTO WINERY VALUES(1,'Columbia Winery','Ursula','Lagadu',695051156);
-INSERT INTO WINERY VALUES(2,'Long Shadows','Elsa','Golitzin',512628281);
-INSERT INTO WINERY VALUES(3,'Chateau Ste. Michelle','Peter','Beswell',984680868);
-INSERT INTO WINERY VALUES(4,'Avennia','Alexander ','Vinall',272414269);
-INSERT INTO WINERY VALUES(5,'Abeja','Thomas','Blonfield',536364213);
-INSERT INTO WINERY VALUES(6,'Delille Cellars','Christopher','McKevany',437839222);
+INSERT INTO WINERY VALUES(1,'Columbia Winery','Ursula','Lagadu');
+INSERT INTO WINERY VALUES(2,'Long Shadows','Elsa','Golitzin');
+INSERT INTO WINERY VALUES(3,'Chateau Ste. Michelle','Peter','Beswell');
+INSERT INTO WINERY VALUES(4,'Avennia','Alexander ','Vinall');
+INSERT INTO WINERY VALUES(5,'Abeja','Thomas','Blonfield');
+INSERT INTO WINERY VALUES(6,'Delille Cellars','Christopher','McKevany');
 
 INSERT INTO EMPLOYEE VALUES(695051156,'Jose','Halls', NULL, 1);
 INSERT INTO EMPLOYEE VALUES(809465589,'Nat','Drever',695051156,1);
